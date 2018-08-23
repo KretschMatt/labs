bb60d3c31045d25d5c3fa0345784ac8296234116
Matt Kretsch <kretsch.matt@gmail.com>
2018-08-23 15:43:46 -0500

    HelloWorldHack

---
+++ HelloWorld.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Matt Kretsch
+ * Collaborator:Ryan Lampe
+ *Date: 08/23/2018
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("My name is Matt Kretsch, I am majoring in computer engineering. My name is Ryan, I am a computer engineering major.\n");
+
+  return 0;
+}
